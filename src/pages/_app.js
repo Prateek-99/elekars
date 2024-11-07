10688c72719ab10fe8098948af627471448fc4a4
Prateek-SIngh1999 <prateek91115@gmail.com>
2024-11-08 00:20:04 +0530

    header

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
 "use client";
+import Header from "@/components/Header";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
